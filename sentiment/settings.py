00a9e7ba411b4157d69becfe55ef4ebab597ac98
Fox-In-A-Box <k_a_jones@hotmail.com>
2022-01-30 08:12:00 -0400

    refactor(setting): Add django-heroku
    docs(requirements.txt): Add django-heroku

--- sentiment/settings.py
+++ sentiment/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -137,3 +138,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = True
+
+django_heroku.settings(locals())
